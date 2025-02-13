62c9b4df546c8678b323bddc1bcc2ac5c0e92f2e
diolemos <pedro.devsc@gamil.com>
2025-02-13 05:42:47 -0300

    update sidebar styles

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,7 +3,7 @@ import { XMarkIcon } from "@heroicons/react/24/solid"; // Tailwind close icon
 export default function Sidebar({ open, onToggleSidebar }) {
   return (
     <aside
-    className={`fixed top-0 left-0 w-1/3 px-0  md:px-8 py-18 md:w-72 h-full bg-white text-blue-900 transition-transform duration-500 ease-in-out shadow-lg
+    className={`md:rounded-tr-2xl  fixed z-50 top-0 left-0 w-2/3 sm:w-2/4 px-0  md:px-8 py-18 md:w-72 h-full bg-white text-blue-900 transition-transform duration-500 ease-in-out shadow-lg
     ${open ? "translate-x-0" : "-translate-x-full"} md:relative md:translate-x-0`}
   >
     {/*  Close Button (Only on Small Screens) */}
@@ -11,7 +11,7 @@ export default function Sidebar({ open, onToggleSidebar }) {
       <XMarkIcon className="h-6 w-6 text-ultraBlue" />
     </button>
   
-    <h2 className="mb-8 p-0   font-bold uppercase md:text-xl text-ultraBlue text-center sm:text-left">Buscas</h2>
+    <h2 className="mb-8 p-0   font-bold uppercase md:text-xl text-ultraBlue  text-center">Buscas</h2>
   </aside>
   );
 }
