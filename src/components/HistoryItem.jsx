195a3b002656865a90a9291f3b3588b08f248a84
diolemos <pedro.devsc@gamil.com>
2025-02-13 13:58:27 -0300

    fix historyItem buttons and style

--- src/components/HistoryItem.jsx
+++ src/components/HistoryItem.jsx
@@ -3,18 +3,9 @@ export default function HistoryItem({ historyItem, onDeleteCep }) {
   return (
     < >
       {/* Clock Icon */}
+      <div className="flex flex-row">
       <ClockIcon className="w-5 h-5 text-gray-500" />
 
-      {/* Date */}
-      <span className="text-sm text-gray-100">{historyItem.date}</span>
-
-      {/* Time */}
-      <span className="text-sm text-gray-200">{historyItem.time}</span>
-
-      {/* Localidade */}
-      <span className="text-sm text-gray-300">{historyItem.localidade}</span>
-
-      {/* Delete Button */}
       
       <span
         onClick={() => onDeleteCep(historyItem.id)}
@@ -22,7 +13,20 @@ export default function HistoryItem({ historyItem, onDeleteCep }) {
         aria-label="Delete search history item"
       >
         <XCircleIcon className="w-5 h-5" />
-      </span>
+      </span></div>
+     
+      {/* Date */}
+      <span className="text-sm">{historyItem.date}</span>
+
+      {/* Time */}
+      <span className="text-sm ">{historyItem.time}</span>
+
+      {/* Localidade */}
+      <span className="text-sm ">{historyItem.localidade}</span>
+
+      {/* Delete Button */}
+      
+      
     </>
   );
 }
