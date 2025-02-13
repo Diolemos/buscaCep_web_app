1dfe3e458b8c62571bb58ac9e3e69fa3ab119e8f
diolemos <pedro.devsc@gamil.com>
2025-02-13 05:43:01 -0300

    Create search bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import './App.css'
 import Sidebar from './components/Sidebar'
 import { Bars3Icon } from "@heroicons/react/24/solid";
+import SearchBar from './components/SearchBar';
 
 
 
@@ -16,16 +17,16 @@ function App() {
   
 
   return (
-    <div className=' bg-ultraBlue h-screen  flex gap-8  mt-8 ' >
+    <div className=' bg-ultraBlue h-screen  flex gap-8  mt-8  px-1' >
+     <Sidebar onToggleSidebar={handleToggleSidebar}  open={open}/>
     {/* Hamburger Button (Only Visible on Small Screens) */}
     <button
         className="md:hidden absolute top-4 right-4 z-50 p-2 bg-white text-ultraBlue rounded-full shadow-lg"
         onClick={handleToggleSidebar}
-      >
-        <Bars3Icon className="h-6 w-6" />
-      </button>
+      > <Bars3Icon className="h-6 w-6" /> </button>
 
-      <Sidebar onToggleSidebar={handleToggleSidebar}  open={open}/>
+      <SearchBar />
+     
     </div>
   )
 }
